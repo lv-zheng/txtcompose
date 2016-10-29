924cd0248aa954ef6d37758d1115ca30bc096e55
Zheng Lv <lv.zheng.2015@gmail.com>
2016-10-29 17:02:57 +0800

    options: fix error message

--- options.cpp
+++ options.cpp
@@ -8,9 +8,7 @@ options::options(int argc, char **argv)
 {
 	assert(argc > 0);
 
-	if (argc < 2)
-		throw bad_cmdline_options{"too few arguments"};
-	if (argc > 3)
+	if (argc > 2)
 		throw bad_cmdline_options{"too many arguments"};
 	
 	rootfile = argv[1];
